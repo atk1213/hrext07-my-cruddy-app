f0179cde7ebc88d106ffc0ebf149233fccebc963
Anthony Kim <tony@Anthonys-MacBook-Pro.local>
2019-02-11 22:09:54 -0800

    Added functionality for added elements. WIP Currently, adding to database and local storage functions. Need to address apis for code snippets and whiteboard. WIP

--- app/app.js
+++ app/app.js
@@ -8,12 +8,29 @@ interact with localstorage
 $(document).ready(function(){
   // this is where we jquery
   //var keyData = 'ourKey'; // going to need to make this dynamic?
+  
+  //init firebase database
+  var config = {
+    apiKey: "AIzaSyCkWeI34gCKLke_L3EvbhEc6z-RqNqb_fw",
+    authDomain: "my-cruddy-app.firebaseapp.com",
+    databaseURL: "https://my-cruddy-app.firebaseio.com",
+    projectId: "my-cruddy-app",
+    storageBucket: "my-cruddy-app.appspot.com",
+    messagingSenderId: "291881914568"
+  };
+  firebase.initializeApp(config);
 
+  var database = firebase.database();
+  var postTitle = "";
+  var postTimestamp;
+  var postContent = {};
+  var postTags = [];
 
-  $('.btn-add').on('click', function(e){
+  $('.btn-add-local').on('click', function(e){
     console.log(e);
-    var keyData = $('.input-key').val();
-    var valueData = $('.input-value').val();
+    var keyData = $('.input-title').val();
+    var valueData = $('.input-textbox').val();
+
     // write to db
     localStorage.setItem(keyData, valueData);
     // read from db
@@ -24,8 +41,32 @@ $(document).ready(function(){
     // if you use backticks ` you can use ${templateLiterals}
     // TODO make this vars make sense across the app
     $('.container-data').html('<div class="display-data-item" data-keyValue="'+ keyData +'">'+valueData+'</div>');
-    $('.input-key').val('');
-    $('.input-value').val('');
+    $('.input-title').val('');
+    $('.input-textbox').val('');
+  });
+
+  $('.btn-add-db').on('click', function(e){
+    // if ($(".input-title").val().trim() == "" || $(".input-textbox" || ".input-snippet" || ".input-whiteboard").val().trim() == ""){
+    if ($(".input-title").val().trim() == "" || $(".input-textbox").val().trim() == ""){
+      alert("Please fill in empty fields before adding")
+    // } else if ($(".input-title").val().trim() !== "" && $(".input-textbox" || ".input-snippet" || ".input-whiteboard").val().trim() !== ""){
+    } else if ($(".input-title").val().trim() !== "" && $(".input-textbox").val().trim() !== "") {
+      postTitle = $(".input-title").val().trim();
+      postTimestamp;
+      postContent["text"] = $(".input-textbox").val().trim();
+      // postContent["whiteboard"] =;
+      // postContent["snippet"] = ;
+      // postTags = ;
+      var newPost = {
+        title: postTitle,
+        // timeStamp: postTimestamp,
+        content: postContent,
+        // tags: postTags,
+    };
+    database.ref().push(newPost);
+    $(".input-title").val("");
+    $(".input-textbox").val("");
+    };
   });
 
 
@@ -44,5 +85,31 @@ $(document).ready(function(){
     localStorage.clear();
     $('.container-data').text('');
   });
+
+  $('.btn-whiteboard').click(function(){
+    // console.log(e.currentTarget.dataset.keyvalue);
+    $(".whiteboard-entry").toggle();
+    console.log("whiteboard!");
+    var sometemp = database.ref().child("temp");
+    sometemp.on("value", snap => console.log(snap.val()));
+  });
+
+  $('.btn-snippet').click(function(){
+    // console.log(e.currentTarget.dataset.keyvalue);
+    $(".snippet-entry").toggle();
+    console.log("snippet!")
+  });
+
+  $('.btn-text').click(function(){
+    // console.log(e.currentTarget.dataset.keyvalue);
+    $(".text-entry").toggle();
+    console.log("text!")
+  });
+
+
+
+
+
+
 
 });
